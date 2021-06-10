9db120ba6cb45d3521ea3a85e736e90b1d97be31
Cristopher <t0ofEr97@gmail.com>
2021-06-10 00:15:10 -0400

    require logeo y autorizacion al buscar usuario por nombre

--- rutas/user.js
+++ rutas/user.js
@@ -14,7 +14,7 @@ router.get('/perfil/buscar/:userId', requiereLogeo, isAuth, (req,res ) => {
 router.put('/perfil/modificar/:userId', requiereLogeo, isAuth, modificarUser)
 router.put('/perfil/comentario/:userId/:userIdComentando', requiereLogeo, isAuth, hacerComentario);
 router.put('/perfil/comentario/respuesta/:userId/:userIdComentando', requiereLogeo, isAuth, respuestaComentario);
-router.get('/perfil/buscar', buscarPorNombre)
+router.get('/perfil/buscar', requireLogeo, isAuth, buscarPorNombre)
 
 //Cada vez que se utilice el parametro userId en la ruta, se ejecutara busacar por id.
 router.param('userId', buscarPorId); 
