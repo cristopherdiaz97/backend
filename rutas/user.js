3faaae6507b889accbcd92b781f7ec2265393f3a
Cristopher <t0ofEr97@gmail.com>
2021-06-15 18:45:46 -0400

    ultimo cambio buscar por nombre usuario

--- rutas/user.js
+++ rutas/user.js
@@ -12,7 +12,7 @@ router.get('/perfil/buscar/:userId', (req, res) => {
 router.put('/perfil/modificar/:userId', requiereLogeo, isAuth, modificarUser)
 router.put('/perfil/comentario/:userId/:userIdComentando', requiereLogeo, isAuth, hacerComentario);
 router.put('/perfil/comentario/respuesta/:userId/:userIdComentando', requiereLogeo, isAuth, respuestaComentario);
-router.get('/perfil/buscar', buscarPorNombre)
+router.post('/perfil/buscar/', buscarPorNombre)
 router.get('/perfil/imagen/:userId', img)
 
 //Cada vez que se utilice el parametro userId en la ruta, se ejecutara busacar por id.
